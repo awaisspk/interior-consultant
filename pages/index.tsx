13894c329a69a166e399d08717d2d203385d8798
awaisspk <iamawaisiqball@gmail.com>
2021-12-09 13:23:54 +0500

    Feat: project done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
+import {Hero} from '@components/Hero/Hero';
+import {Layout} from '@components/Layout';
 import type {NextPage} from 'next';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return (
+    <>
+      <Head>
+        <title>modern interior</title>
+      </Head>
+      <Layout header footer>
+        <Hero />
+      </Layout>
+    </>
+  );
 };
 
 export default Home;
